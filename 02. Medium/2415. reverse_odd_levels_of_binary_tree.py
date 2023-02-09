b55bc25da7bf28f36a67cb527f10831b6367db84
dandr94 <d.andreev1994@gmail.com>
2023-02-09 18:24:54 +0200

    Added 2415

---
+++ 02. Medium/2415. reverse_odd_levels_of_binary_tree.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        def dfs(node_1: TreeNode, node_2: TreeNode, curr_level: int) -> None:
+            if not node_1 or not node_2:
+                return
+
+            if curr_level % 2 != 0:
+                node_1.val, node_2.val = node_2.val, node_1.val
+
+            dfs(node_1.left, node_2.right, curr_level + 1)
+            dfs(node_1.right, node_2.left, curr_level + 1)
+
+        dfs(root.left, root.right, 1)
+
+        return root
