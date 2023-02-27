5c06303b07894a65aae37171598127458f84e296
dandr94 <d.andreev1994@gmail.com>
2023-02-27 22:28:21 +0200

    Add problem desc link

--- 01. Easy/2469. convert_the_temperature.py
+++ 01. Easy/2469. convert_the_temperature.py
@@ -6,8 +6,8 @@ class Solution:
         return [celsius + 273.15, celsius * 1.80 + 32]
 
 
-
-
 sol = Solution()
 print(sol.convertTemperature(36.50))
 print(sol.convertTemperature(122.11))
+
+# Problem - https://leetcode.com/problems/convert-the-temperature/description/
