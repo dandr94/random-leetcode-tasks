d2522b09dc72e6f829a5381bf0c81c506b334610
dandr94 <d.andreev1994@gmail.com>
2023-03-02 05:57:31 +0200

    Add problem link

--- 02. Medium/240. search_a_2D_matrix_II.py
+++ 02. Medium/240. search_a_2D_matrix_II.py
@@ -23,3 +23,5 @@ print(sol.searchMatrix(
 print(sol.searchMatrix(
     matrix=[[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]],
     target=20))
+
+# Problem - https://leetcode.com/problems/search-a-2d-matrix-ii/description/
