13e6fd2fda73722ac4dc397e4a60d0f3ef8d8b9a
dandr94 <d.andreev1994@gmail.com>
2023-05-25 15:35:29 +0300

    Add problem url

--- 01. Easy/338. counting_bits.py
+++ 01. Easy/338. counting_bits.py
@@ -3,11 +3,15 @@ from typing import List
 class Solution:
     def countBits(self, n: int) -> List[int]:
         counter = [0]
+
         for i in range(1,n+1):
-            counter.append(counter[i>>1] + i%2)
+            counter.append(counter[i>>1] + i % 2)
+
         return counter 
 
 
 sol = Solution()
 print(sol.countBits(n=2))
 print(sol.countBits(n=5))
+
+# Problem - https://leetcode.com/problems/counting-bits/description/
