8baf2035989a849d6999e29bbe96aeb39a0d85a9
dandr94 <d.andreev1994@gmail.com>
2023-06-10 16:21:04 +0300

    Added 242

---
+++ 01. Easy/242. valid_anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+
+sol = Solution()
+print(sol.isAnagram(s="anagram", t="nagaram"))
+print(sol.isAnagram(s="rat", t="car"))
+
+# Problem - https://leetcode.com/problems/valid-anagram/
