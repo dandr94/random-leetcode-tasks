783e951a6b927941f4600b30bfb775cfee427134
dandr94 <d.andreev1994@gmail.com>
2025-04-23 22:44:43 +0300

    [Hash]: Added medium Hash task: 560

--- 02. Medium/02. Hash/560. subarray_sum_equals_k.py
+++ 02. Medium/02. Hash/560. subarray_sum_equals_k.py
@@ -5,18 +5,23 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         res = 0
         prefix_sum = 0
-        _dict = {0: 1}
+        sum_dict = {0: 1}
 
         for num in nums:
             prefix_sum += num
-            if prefix_sum - k in _dict:
-                res = res + _dict[prefix_sum - k]
-            _dict[prefix_sum] = _dict.get(prefix_sum, 0) + 1
+
+            if prefix_sum - k in sum_dict:
+                res += sum_dict[prefix_sum - k]
+
+            sum_dict[prefix_sum] = sum_dict.get(prefix_sum, 0) + 1
+
         return res
 
 
 sol = Solution()
 print(sol.subarraySum(nums=[1, 1, 1], k=2))
 print(sol.subarraySum(nums=[1, 2, 3], k=3))
+print(sol.subarraySum(nums=[1], k=1))
 
-# Problem - https://leetcode.com/problems/subarray-sum-equals-k/
+# Medium
+# https://leetcode.com/problems/subarray-sum-equals-k/
