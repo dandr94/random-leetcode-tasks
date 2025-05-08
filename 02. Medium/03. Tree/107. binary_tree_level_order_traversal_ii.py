599c89556fb06dbfdfb104cd8778c330e3a5c287
dandr94 <d.andreev1994@gmail.com>
2025-05-08 22:23:27 +0300

    [Tree]: Added medium Tree tasks: 102, 107

---
+++ 02. Medium/03. Tree/107. binary_tree_level_order_traversal_ii.py
@@ -0,0 +1,55 @@
+from collections import deque
+from typing import Optional, List
+
+from helper import build_tree
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        res = []
+        queue = deque([root])
+
+        while queue:
+            level_vals = []
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level_vals.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            res.append(level_vals)
+
+        return res[::-1]
+
+
+sol = Solution()
+
+tree = build_tree([3, 9, 20, None, None, 15, 7])
+
+print(sol.levelOrderBottom(tree))
+
+tree = build_tree([1])
+
+print(sol.levelOrderBottom(tree))
+
+tree = build_tree([])
+
+print(sol.levelOrderBottom(tree))
+
+# Medium
+# https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
